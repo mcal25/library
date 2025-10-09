7278905f5643bbb34c45dc1be7c07b00d5893fee
mcal25 <225284010+mcal25@users.noreply.github.com>
2025-10-09 17:54:39 -0400

    Create object constructor and basic skeleton

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('testing');
+const daLibrary = [];
+
+
+function Book(title, author, pages, publishedDate) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.publishedDate = publishedDate;
+    this.id = crypto.randomUUID();
+}
+
+function makeAndStoreBook(title, author, publishedDate, pages) {
+    daLibrary.push(new Book(title, author, publishedDate, pages));
+}
+
+makeAndStoreBook('Lawd of da Ringz', 'J-Dog', 2500, 1969);
+
+function generateLibrary() {
+    // loop through lib arr & displays each book on page
+}
+
+
+console.log(daLibrary);
