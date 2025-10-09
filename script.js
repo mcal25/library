1ed1149ab72127d2c22dab153a8326e751be8836
mcal25 <225284010+mcal25@users.noreply.github.com>
2025-10-09 19:57:59 -0400

    Refine styling; Add button

--- script.js
+++ script.js
@@ -14,10 +14,38 @@ function makeAndStoreBook(title, author, publishedDate, pages) {
 }
 
 makeAndStoreBook('Lawd of da Ringz', 'J-Dog', 2500, 1969);
+makeAndStoreBook('Berry Potta', 'Angry Lady', 1500, 2000);
+makeAndStoreBook('Bilight', 'Stiffy Meijer', 100, 2012);
+makeAndStoreBook('Stormsight Awwkhivez', 'Casper Sandersun', 100, 2012);
+makeAndStoreBook('Fourth Bling', 'Jabekka Jarros', 100, 2012);
+makeAndStoreBook('Dungeon Crawler Gary the Snail', 'Matthew Deezamin', 100, 2012);
+makeAndStoreBook('Dungeon Crawler Gary the Snail', 'Matthew Deezamin', 100, 2012);
+makeAndStoreBook('Dungeon Crawler Gary the Snail', 'Matthew Deezamin', 100, 2012);
+makeAndStoreBook('Dungeon Crawler Gary the Snail', 'Matthew Deezamin', 100, 2012);
+
+
+
+
+
+const bookList = document.getElementById('book-list');
+console.log(bookList);
+
+
+
 
 function generateLibrary() {
     // loop through lib arr & displays each book on page
+    for(let i = 0; i < daLibrary.length; i++) {
+        const card = document.createElement('div');
+        card.classList.add('book-card');
+        bookList.append(card);
+        card.textContent = 'yeehaw';   
+    }
 }
+
+generateLibrary();
 
 
 console.log(daLibrary);
+
+console.log(bookList);
