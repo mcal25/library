d1998d7bc20a52a98bb88e7599c4a21a332656dc
mcal25 <225284010+mcal25@users.noreply.github.com>
2025-10-22 17:55:59 -0400

    Make working "Remove" book button

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-const daLibrary = [];
+let daLibrary = [];
 
 
-function Book(title, author, pages, publishedDate) {
+function Book(title, author, pages, publishedDate, finishedReading) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.publishedDate = publishedDate;
+    this.finishedReading = finishedReading;
     this.id = crypto.randomUUID();
-    this.toggleRead = function() {
+    this.toggleRead = function () {
         this.read == true ? false : true;
     }
 }
 
-function makeAndStoreBook(title, author, publishedDate, pages) {
-    daLibrary.push(new Book(title, author, publishedDate, pages));
+function makeAndStoreBook(title, author, pages, publishedDate, finishedReading) {
+    daLibrary.push(new Book(title, author, pages, publishedDate, finishedReading));
 }
+
+// order my props are in is how I have to setup my form data ^^
 
 makeAndStoreBook('Lawd of da Ringz', 'J-Dog', 2500, 1969);
 makeAndStoreBook('Berry Potta', 'Angry Lady', 1500, 2000);
@@ -31,20 +34,33 @@ makeAndStoreBook('Dungeon Crawler Gary the Snail', 'Matthew Deezamin', 100, 2012
 
 
 const bookList = document.getElementById('book-list');
-console.log(bookList);
+// console.log(bookList);
 
 
 
 
 function generateLibrary() {
     // loop through lib arr & displays each book on page
-    for(let i = 0; i < daLibrary.length; i++) {
+    while (bookList.firstChild) {
+        bookList.removeChild(bookList.firstChild);
+    };
+    for (let i = 0; i < daLibrary.length; i++) {
         const card = document.createElement('div');
         card.classList.add('book-card');
         bookList.append(card);
         card.textContent = 'yeehaw';
         const bookTitle = document.createElement('h1');
         card.append(bookTitle);
+        card.setAttribute('data-book-id', daLibrary[i].id)
+        const removeBookButton = document.createElement('button');
+        removeBookButton.textContent = 'Remove';
+        removeBookButton.classList.add('remove-book-button');
+        removeBookButton.addEventListener('click', () => {
+            console.log(this.id)
+            daLibrary = daLibrary.filter(book => book.id != daLibrary[i].id);
+            generateLibrary();
+        });
+        card.append(removeBookButton);
         bookTitle.textContent = daLibrary[i].title
     }
 }
@@ -54,16 +70,37 @@ generateLibrary();
 const newBookButton = document.getElementById('new-book-button');
 const dialog = document.getElementById('new-book-dialog');
 const closeDialogButton = document.getElementById('close-dialog-button');
+const submitBookButton = document.getElementById('submit-book-button');
 
 newBookButton.addEventListener('click', () => {
     dialog.showModal();
 })
+
+submitBookButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const publishedDate = document.getElementById('published-date').value;
+    const finishedReading = document.getElementById('finished-reading').value;
+    // const formData = {
+    //     title: title,
+    //     author: author,
+    //     pages: pages,
+    //     publishedDate: publishedDate,
+    //     finishedReading: finishedReading,
+    // };
+    // console.log(formData);
+    makeAndStoreBook(title, author, pages, publishedDate, finishedReading);
+    generateLibrary();
+})
+
 
 closeDialogButton.addEventListener('click', () => {
     dialog.close();
 })
 
 
-console.log(daLibrary);
+// console.log(daLibrary);
 
-console.log(bookList);
+// console.log(bookList);
